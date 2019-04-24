cc5fc17336a0424e1ad3b367a5cce9250aaf73f3
dantemazza <47069410+dantemazza@users.noreply.github.com>
2019-04-24 12:30:17 -0400

    enabled move disambiguation for captures

--- src/chess/moveNotator.java
+++ src/chess/moveNotator.java
@@ -22,7 +22,7 @@ public class moveNotator {
 		
 		spec:{
 			if(piece instanceof Queen || piece instanceof Rook) this.specHorzVert(where, piece, board);
-			if(piece instanceof Knight) this.specKnightFile(where, (Knight)piece, board);
+			if(piece instanceof Knight) this.specKnightFileRank(where, (Knight)piece, board);
 			if(specBoth) {
 				moveNotation += this.getFile(piece.getPosition().getX(), blackTop);
 				moveNotation += this.getRank(piece.getPosition().getY(), blackTop);
@@ -33,14 +33,17 @@ public class moveNotator {
 		}
 		
 		
-		if(wasCapture) { moveNotation += "x";
-		
-		if(piece instanceof Pawn) moveNotation += this.getFile(where.getX(), blackTop);
+		if(wasCapture) { 
+			moveNotation += "x";
+			if(piece instanceof Pawn) moveNotation += this.getFile(where.getX(), blackTop);
 		}
 		if(!(piece instanceof Pawn)) moveNotation += this.getFile(where.getX(), blackTop);
 		moveNotation += this.getRank(where.getY(), blackTop);
 		
-		if(wasMate) { moveNotation += "#"; return; }
+		if(wasMate){ 
+			moveNotation += "#"; 
+			return; 
+			}
 		if(wasCheck) moveNotation += "+";
 		
 		if(moveNotation.length() == 2 && moveNotation.charAt(0) == 'f' && board.whoseTurn == 1) moveNotation += "\t";
@@ -55,23 +58,23 @@ public class moveNotator {
 	public void specHorzVert(Vector pos, Piece piece, Board board) {
 		
 		int x = pos.getX(), y = pos.getY();
-		if(pos.getX()<6) {
-		for(int a = pos.getX(); a<=7; a++){
+		if(pos.getX()<7) {
+		for(int a = pos.getX()+1; a<=7; a++){
 			if(board.isOccupied(a,y)) {
 				if(!board.getSquare(a,y).side.equals(piece.side) || board.getSquare(a,y).getClass() != piece.getClass() || piece.getPosition().equals(a, y)) break;
 				if(!board.validateMove(board.getSquare(a,y), pos, false)) specFile = true;
 			}
 		}}
-		if(pos.getX()>1) {
-		for(int a = pos.getX(); a>=0; a--){ 
+		if(pos.getX()>0) {
+		for(int a = pos.getX()-1; a>=0; a--){ 
 			if(board.isOccupied(a,y)) {
 				if(!board.getSquare(a,y).side.equals(piece.side) || board.getSquare(a,y).getClass() != piece.getClass() || piece.getPosition().equals(a, y)) break;
 				if(!board.validateMove(board.getSquare(a,y), pos, false)) specFile = true;
 			}
 		}}
 		
-		if(pos.getY()<6) {
-		for(int a = pos.getY(); a<=7; a++){
+		if(pos.getY()<7) {
+		for(int a = pos.getY()+1; a<=7; a++){
 			if(board.isOccupied(x,a)) {
 				if(!board.getSquare(x,a).side.equals(piece.side) || board.getSquare(x,a).getClass() != piece.getClass() || piece.getPosition().equals(x, a)) break;
 				if(!board.validateMove(board.getSquare(x,a), pos, false)) {
@@ -80,8 +83,8 @@ public class moveNotator {
 				}
 			}
 		}}
-		if(pos.getY()>1) {
-		for(int a = pos.getY(); a>=0; a--){
+		if(pos.getY()>0) {
+		for(int a = pos.getY()-1; a>=0; a--){
 			if(board.isOccupied(x,a)) {
 				if(!board.getSquare(x,a).side.equals(piece.side) || board.getSquare(x,a).getClass() != piece.getClass() || piece.getPosition().equals(x, a)) break;
 				if(!board.validateMove(board.getSquare(x,a), pos, false)) {
@@ -90,16 +93,19 @@ public class moveNotator {
 				}		
 			}
 		}}	
-		specBoth = specRank & specFile;		
+		specBoth = specRank && specFile;		
 	}
-	public void specKnightFile(Vector pos, Knight knight, Board board) {
+	public void specKnightFileRank(Vector pos, Knight knight, Board board) {
 		int x = pos.getX(), y = pos.getY();
 		for(int i=0; i<8; i++) {
 			if(x+Board.xC[i] > 7 || y+Board.yC[i] > 7 || x+Board.xC[i] < 0 || y+Board.yC[i] < 0) continue;
 			if(board.isOccupied(x+Board.xC[i],y+Board.yC[i])) {
 				if(!board.getSquare(x+Board.xC[i],y+Board.yC[i]).side.equals(knight.side) || board.getSquare(x+Board.xC[i],y+Board.yC[i]).getClass() != knight.getClass()
 				|| knight.getPosition().equals(x+Board.xC[i],y+Board.yC[i])) continue;
-				if(!board.validateMove(board.getSquare(x+Board.xC[i],y+Board.yC[i]), pos, false)) specFile = true;	
+				if(!board.validateMove(board.getSquare(x+Board.xC[i],y+Board.yC[i]), pos, false)) {
+					if(knight.getPosition().getX() != x+Board.xC[i]) specFile = true;	
+					else specRank = true;
+				}
 			}
 		}		
 	}
@@ -107,19 +113,18 @@ public class moveNotator {
 	public char getFile(int x, boolean isBT) {
 		int b=0;
 		if(isBT) {
-		for(char a = 'a'; a<='h'; a++) {
-			if(b == x) return a;
-			b++;
-		}}else{ b = 7;
-		for(char a = 'a'; a<='h'; a++) {
-			if(b == x)  return a;
-			b--;
-		}	
-	} return 'z';
+			for(char a = 'a'; a<='h'; a++) {
+				if(b == x) return a;
+				b++;
+			}}else{ b = 7;
+			for(char a = 'a'; a<='h'; a++) {
+				if(b == x)  return a;
+				b--;
+			}	
+		} return 'z';
 	}
 	public String getRank(int y, boolean isBT) {
-		if(!isBT) return Integer.toString(y+1);
-		else return Integer.toString(8-y);
+		return isBT ? Integer.toString(8-y) : Integer.toString(y+1);
 	}
 	
 }
